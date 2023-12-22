50e38479bfffff02d37b0931138ee1f42723de37
AwadhRajPatel <144754071+AwadhRajPatel@users.noreply.github.com>
2023-12-22 09:58:03 +0000

    02_dataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all js code as newer version
+// alert(3 + 3)// we are using node js , not brower
+
+console.log("Raja")
+
+let name = "Raja"
+let age = 18
+let isLoggedIn = false
+
+// Primitive data type
+//  1.number ,2.bigint ,3.string, 4.boolean, 5.null, 6.undefined, 7.symbol
+console.log(typeof  null);
+// null // object
