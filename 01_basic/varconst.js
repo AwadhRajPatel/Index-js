a595bb6e54036ef6df56536bc5f0efbf63c92ec7
AwadhRajPatel <144754071+AwadhRajPatel@users.noreply.github.com>
2023-12-22 09:59:12 +0000

    Variabile

---
+++ 01_basic/varconst.js
@@ -0,0 +1,14 @@
+const accountId =  144553
+let accountEmail = "Awadh@goole.com"
+var accountPassword = "54478"
+accountCity = "LUCKNOW"
+let accountState;
+
+// accountId = 2 //not  allowed
+
+accountEmail = "Patel@gmail.com"
+accountPassword = "654528"
+accountCity = "Kunda"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
