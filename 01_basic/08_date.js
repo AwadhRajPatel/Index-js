899c2ba600d62bd07ccff02b537ea8d65139ee8a
Awadh Raj Patel <144754071+AwadhRajPatel@users.noreply.github.com>
2023-12-27 06:30:33 +0000

    date and time

--- 01_basic/08_date.js
+++ 01_basic/08_date.js
@@ -1,4 +1,34 @@
 // DATE AND TIMES PROGRAM
 
 let myDate = new Date()
-console.log(myDate.toString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.getHours())
+// console.log(myDate.getTimezoneOffset())
+// console.log(myDate.toJSON());
+// console.log(myDate.to)
+// console.log(typeof myDate);
+
+// let myCreatedDate =  new  Date(2023,11,23,5,3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+let myCreatedDate = new Date ("01-14-2023")
+// console.log(myCreatedDate.toLocaleDateString)
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+//  console.log(myTimeStamp);
+//  console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let  newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+console.log(newDate.getTimezoneOffset());
+
+newDate.toLocaleDateString(`default`,{
+    weekday: "long"
+})
