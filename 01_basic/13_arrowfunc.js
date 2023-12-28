9c3747dea8d30c0d00ba3b7d8b770bd761dba84b
Awadh Raj Patel <144754071+AwadhRajPatel@users.noreply.github.com>
2023-12-28 08:43:08 +0000

    ARROWFUNCTION

---
+++ 01_basic/13_arrowfunc.js
@@ -0,0 +1,20 @@
+const user = {
+    username: "AwadhRaj",
+    price: 999,
+    WelcomeMessage: function(){
+        console.log(`${this.username} , welcome to website `)
+        console.log(this);
+    }
+}
+// user.WelcomeMessage()
+// user.username = "sam"
+// user.WelcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "AwadhRaj"
+//     console.log(this.user);
+// }
+// chai()
+
