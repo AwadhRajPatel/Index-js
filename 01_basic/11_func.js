fa458731d4a9b0f330cd3fa1455a97fef9ca6461
Awadh Raj Patel <144754071+AwadhRajPatel@users.noreply.github.com>
2023-12-28 05:30:12 +0000

    FunctionJS

--- 01_basic/11_func.js
+++ 01_basic/11_func.js
@@ -40,4 +40,30 @@ function loginUserMessage(username = "sam"){
 
 // console.log(loginUserMessage("AwadhRaj"))
 
-console.log(loginUserMessage("Raja"))
+// console.log(loginUserMessage("Raja"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500 ,1000))
+
+const user = {
+    username: "Awadhraj",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log (`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
